e5c174aa54bd0a43b7606a56f815f2135ca8b525
libingzeng <libingzeng123@gmail.com>
2018-07-26 14:25:04 +0800

    Initial Commit

---
+++ RayTraceGroundUp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTraceGroundUp
+//
+//  Created by libingzeng on 2018/7/26.
+//  Copyright © 2018 libingzeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
